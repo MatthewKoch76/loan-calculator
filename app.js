0507af3d967fd53134809673d12add54b4b517c4
MatthewKoch76 <37314071+MatthewKoch76@users.noreply.github.com>
2019-03-13 08:48:40 -0500

    finished

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e){
+  document.getElementById('results').style.display = 'none';
+  document.getElementById('loading').style.display = 'block';
+  setTimeout(calculateResults, 2000);
 
-function calculateResults(e) {
+  e.preventDefault();
+});
+
+function calculateResults() {
   const amount = document.getElementById('amount');
   const interest = document.getElementById('interest');
   const years = document.getElementById('years');
@@ -19,14 +25,21 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+
+    document.getElementById('results').style.display = 'block';
+
+    document.getElementById('loading').style.display = 'none';
   } else {
     showError('Please check nums, bud');
   }
-
-  e.preventDefault();
 }
 
 function showError(error){
+
+  document.getElementById('results').style.display = 'none';
+
+  document.getElementById('loading').style.display = 'none';
+
   const errorDiv = document.createElement('div');
   
   const card = document.querySelector('.card');
